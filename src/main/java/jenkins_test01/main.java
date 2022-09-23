ed2d80c75b3f24423ad516f8786fe1b1acc5e4ca
Javier Sastron Artigas <jsastron@emeal.nttdata.com>
2022-09-23 08:43:38 +0200

    test

---
+++ src/main/java/jenkins_test01/main.java
@@ -0,0 +1,13 @@
+package jenkins_test01;
+
+/**
+ * Hello world!
+ *
+ */
+public class main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
